8dbe1c34d23c66f8c230c4c89336daccbea6a966
Balázs Zsolt Csőke <csoke.balazs@verebelyszki.hu>
2024-03-18 17:52:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Potnhatárok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hány pontja lett a 100-ból?: ");
+            int pontszam = int.Parse(Console.ReadLine());
+
+            if (pontszam < 0 || pontszam > 100)
+            {
+                Console.WriteLine("Az adat hibás!");
+            }
+            else if (pontszam > 0 & pontszam < 50)
+            {
+                Console.WriteLine("Az érdemjegye: 1");
+            }
+            else if (pontszam >= 50 & pontszam < 65)
+            {
+                Console.WriteLine("Az érdemjegye: 2");
+            }
+            else if (pontszam >= 65 && pontszam < 80)
+            {
+                Console.WriteLine("Az érdemjegye: 3");
+            }
+            else if (pontszam >= 80 && pontszam < 90)
+            {
+                Console.WriteLine("Az érdemjegye: 4");
+            }
+            else if (pontszam >= 90)
+            {
+                Console.WriteLine("Az érdemjegye: 5");
+            }
+        }
+    }
+}
